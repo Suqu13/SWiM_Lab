dfd5837c968822df92619269b6cace0e0c94b23e
Suqu13 <jgarstka3@gmail.com>
2019-05-23 15:35:50 +0200

    tags bug fix

--- SWiM_2/app/src/main/java/com/example/swim_2/ImageAdapter.kt
+++ SWiM_2/app/src/main/java/com/example/swim_2/ImageAdapter.kt
@@ -42,7 +42,7 @@ class ImageAdapter(private val images: ArrayList<Image>) : RecyclerView.Adapter<
                             .toTypedArray()
                             .take(3)
                         holder.tags.text = tags.joinToString(" #", prefix = "#")
-                        images[holder.adapterPosition].tags.addAll(ArrayList(tags))
+                        images[holder.adapterPosition].tags = (ArrayList(tags))
                     }
             }
             override fun onPrepareLoad(placeHolderDrawable: Drawable?) {}
@@ -50,7 +50,7 @@ class ImageAdapter(private val images: ArrayList<Image>) : RecyclerView.Adapter<
             override fun onBitmapFailed(e: Exception?, errorDrawable: Drawable?) {
                 holder.image.setImageResource(R.drawable.error)
                 val errorTag = holder.itemView.resources.getString(R.string.error_tag)
-                images[holder.adapterPosition].tags.add(errorTag.substring(startIndex = 1))
+                images[holder.adapterPosition].tags = arrayListOf(errorTag.substring(startIndex = 1))
                 holder.tags.text = errorTag
                 holder.name.text = holder.itemView.resources.getString(R.string.error_name)
             }
