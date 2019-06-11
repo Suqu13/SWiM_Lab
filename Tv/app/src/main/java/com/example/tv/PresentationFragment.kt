6473cefe4d8476bb30f31c3f77c9045040bd7f62
Suqu13 <jgarstka3@gmail.com>
2019-06-11 17:00:56 +0200

    tv

---
+++ Tv/app/src/main/java/com/example/tv/PresentationFragment.kt
@@ -0,0 +1,13 @@
+package com.example.tv
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+//class PresentationFragment : Fragment() {
+////    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+////        val view = inflater
+////    }
+//}
